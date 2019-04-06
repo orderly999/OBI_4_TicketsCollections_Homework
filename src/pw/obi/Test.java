6d6e9e55ffebd370c6203c6f3b64d0a6122989cc
kkruk <orderly999@gmail.com>
2019-04-06 10:07:47 +0200

    Dodanie klas

--- src/pw/obi/Test.java
+++ src/pw/obi/Test.java
@@ -8,16 +8,22 @@ public class Test {
 	public static void main(String[] args) {
 
 	Flight flight = new Flight(180, "WAW", "PAR", 99.99);	
+	SystemReservation sys = new SystemReservation();
 	
+	Passenger passenger1 = new Passenger("Kowalski Jan", "87121289234");
+	sys.addPassenger(passenger1);
 	
 	for (int i = 0; i < 190; i++ )
 	{
-		Passenger passenger = new Passenger(generateRndomName(), generateRndomID());	
-		SystemReservation.out.println(flight.reserveTicket(passenger));
+		Passenger passenger = new Passenger(generateRndomName(), generateRndomID());
+		sys.addPassenger(passenger);
+		System.out.println(flight.reserveTicket(passenger));
 	}
 	
-	SystemReservation.out.println("Zarezerwowano łącznie " + flight.getNumberOfReservedTickets() + " biletów za kwotę: " + flight.getPriceForTicket()
+	System.out.println("Zarezerwowano łącznie " + flight.getNumberOfReservedTickets() + " biletów za kwotę: " + flight.getPriceForTicket()
 	+ "$ na tym locie linie lotnicze zarobią " + (double)(flight.getNumberOfReservedTickets()*flight.getPriceForTicket()) + " $");
+	
+	System.out.println("Nazwisko wyszukanego pasażera to: " + sys.findPassengerByID("87121289234").getName());
 
 	}
 	
